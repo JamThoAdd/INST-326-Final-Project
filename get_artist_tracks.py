7ce9f6d0f3666308f17a263840653a714f10bdbc
weilumtang <50188602+weilumtang@users.noreply.github.com>
2019-05-16 13:17:20 -0400

    Update get_artist_tracks.py

--- get_artist_tracks.py
+++ get_artist_tracks.py
@@ -1,8 +1,11 @@
+import csv
+
 def get_artist_tracks():
     """
-    returns a list of all of the songs an artist has and what region on the charts
+    Allows user to search for an artist 
+    If entry in data matches the inputed artist, appends song to a list
+    Returns a list of all of the songs an artist has on the charts
     """
-    import csv
     fh = open("data.csv")
     info = csv.DictReader(fh)
     user_input = input("type an artist: ")
@@ -12,5 +15,6 @@ def get_artist_tracks():
             song_entry = entries['Track Name']
             if song_entry not in songs:
                 songs.append(song_entry)
-                print(songs)
+     print(songs)
+    
 get_artist_tracks()
