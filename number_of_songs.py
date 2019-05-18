c5e99301116af2b8040e12caceb75b234fdbaf43
intisar505 <35035930+intisar505@users.noreply.github.com>
2019-05-17 22:04:53 -0400

    Update number_of_songs.py

--- number_of_songs.py
+++ number_of_songs.py
@@ -1,9 +1,10 @@
 
 def get_number_of_songs():
     """
-    returns a list of all of the songs an artist has on the charts
+    returns a list of all of the songs an artist has and what region on the charts
     """
     import csv
+    #open the file for Spotify top 2018 songs
     fh = open("top2018.csv")
     info = csv.DictReader(fh)
     songs = []
@@ -12,11 +13,18 @@ def get_number_of_songs():
     user_input = input("type an artist: ")
     for entries in info:
         if user_input == entries['artists']:
+            #adds song to artist's song list
             songs.append(entries['name'])
+            #adds 1 to the artist"s total songs count
             song_count += 1
     for entries in info:
         if user_input != entries['artists']:
             print("%s had 0 songs in the Spotify Top 100 Chart of 2018" %user_input)
     print(songs)
     print("%s had " %user_input + str(song_count) + " songs in the Spotify Top 100 Chart of 2018")
-get_number_of_songs()
+# THIS IS A TEST USING ASSERT (artist name must be in the data set in order to have info)
+    assert user_input not in entries, "artist must be in data set"
+
+
+if __name__ == "__main__":
+    get_number_of_songs()
